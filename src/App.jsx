32eaa6aba67421cde973ea8e885a63db39f135de
Hernandez Ordonez <2383131@cognizant.com>
2025-01-08 11:20:59 -0600

    feature(separete into different components, better implementation of passing props)

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,18 @@
-import { CORE_CONCEPTS } from './data';
-import { useState } from 'react';
-
 import Header from './components/Header/Header';
-import CoreConcept from './components/CoreConcept';
-import TabButton from './components/TabButton';
-import { EXAMPLES } from './data';
+import CoreConcepts from './components/CoreConcepts';
+import Examples from './components/Examples';
+
 function App() {
-  const [getSelected, setSelected] = useState();
-  const handleClick = (selectedButton) => {
-    setSelected(selectedButton);
-  }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
 
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-
-            ))}
-          </ul>
-        </section>
-        <section id='examples'>
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={getSelected === 'components'} onSelectTab={() => handleClick('components')}>Components </TabButton>
-            <TabButton isSelected={getSelected === 'jsx'} onSelectTab={() => handleClick('jsx')}>JSX </TabButton>
-            <TabButton isSelected={getSelected === 'props'} onSelectTab={() => handleClick('props')}>Props </TabButton>
-            <TabButton isSelected={getSelected === 'state'} onSelectTab={() => handleClick('state')}>State </TabButton>
-          </menu>
-          <div id="tab-content">
-            {!getSelected ? <p>Please select a topic</p> :
-              <div>
-                <h3>{EXAMPLES[getSelected].title}</h3>
-                <p>
-                  {EXAMPLES[getSelected].description}
-                </p>
-                <pre>
-                  <code>
-                    {EXAMPLES[getSelected].code}
-                  </code>
-                </pre>
-              </div>
-            }
-          </div>
-        </section>
+        <CoreConcepts></CoreConcepts>
+        <Examples></Examples>
       </main>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Examples.jsx
@@ -0,0 +1,37 @@
+import TabButton from './TabButton';
+import { EXAMPLES } from '../data';
+import { useState } from 'react';
+import Section from './Section';
+
+export default function Examples() {
+  const [getSelected, setSelected] = useState();
+  const handleClick = (selectedButton) => {
+    setSelected(selectedButton);
+  }
+
+  return (
+    <Section title={'Some examples here'} id='examples'>
+      <menu>
+        <TabButton isSelected={getSelected === 'components'} onClick={() => handleClick('components')}>Components </TabButton>
+        <TabButton isSelected={getSelected === 'jsx'} onClick={() => handleClick('jsx')}>JSX </TabButton>
+        <TabButton isSelected={getSelected === 'props'} onClick={() => handleClick('props')}>Props </TabButton>
+        <TabButton isSelected={getSelected === 'state'} onClick={() => handleClick('state')}>State </TabButton>
+      </menu>
+      <div id="tab-content">
+        {!getSelected ? <p>Please select a topic</p> :
+          <div>
+            <h3>{EXAMPLES[getSelected].title}</h3>
+            <p>
+              {EXAMPLES[getSelected].description}
+            </p>
+            <pre>
+              <code>
+                {EXAMPLES[getSelected].code}
+              </code>
+            </pre>
+          </div>
+        }
+      </div>
+    </Section>
+  )
+}
