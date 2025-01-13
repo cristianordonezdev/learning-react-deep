254d047be3912113cf5488196afc62fad4c56718
Hernandez Ordonez <2383131@cognizant.com>
2025-01-13 16:51:19 -0600

    five apps

--- five-app/src/components/Login.js
+++ five-app/src/components/Login.js
@@ -1,17 +1,33 @@
+import { useState } from "react"
+
 export default function Login() {
+  const [form, setForm] = useState({
+    email: "",
+    password: ""
+  })
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Hello world", form)
+  }
+ 
+  function handleOnChangeForm(id, value) {
+    setForm((old_values) => ({...old_values, [id]: value}))
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={(event) => handleOnChangeForm("email", event.target.value)} value={form.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={(event) => handleOnChangeForm("password", event.target.value)} value={form.password}/>
         </div>
       </div>
 
