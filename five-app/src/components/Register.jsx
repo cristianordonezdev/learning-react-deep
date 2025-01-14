7c530836da06451b26c5dab8e18bd78e10d8cece
Hernandez Ordonez <2383131@cognizant.com>
2025-01-14 11:42:00 -0600

    feature(validations with usestate)

--- five-app/src/components/Register.jsx
+++ five-app/src/components/Register.jsx
@@ -6,6 +6,14 @@ export default function Register() {
     password: ""
   })
 
+  const [stateForm, setStateForm] = useState({
+    email: false,
+    password: false
+  })
+
+  const emailIsValid = stateForm.email && !form.email.includes("@")
+  const passwordIsValid = stateForm.password && form.password.length < 4;
+
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -14,6 +22,14 @@ export default function Register() {
  
   function handleOnChangeForm(id, value) {
     setForm((old_values) => ({...old_values, [id]: value}))
+    setStateForm((old_values) => ({...old_values, [id]: false}))
+  }
+
+  function handleOnBlurForm(id, value) {
+    setStateForm((old_value) => ({
+      ...old_value,
+      [id]: value
+    }))
   }
 
   return (
@@ -23,13 +39,21 @@ export default function Register() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onChange={(event) => handleOnChangeForm("email", event.target.value)} value={form.email}/>
-
+          <input id="email" type="email" name="email" 
+            onChange={(event) => handleOnChangeForm("email", event.target.value)} value={form.email}
+            onBlur={(event) => handleOnBlurForm("email", event.target.value)}
+          />
+          {emailIsValid && <span className="control-error">Please enter a valid email</span>}
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" onChange={(event) => handleOnChangeForm("password", event.target.value)} value={form.password}/>
+          <input id="password" type="password" name="password" 
+            onChange={(event) => handleOnChangeForm("password", event.target.value)}
+            onBlur={(event) => handleOnBlurForm("password", event.target.value)}
+            value={form.password}
+          />
+          {passwordIsValid && <span className="control-error">Please enter a strong password</span>}
 
         </div>
       </div>
