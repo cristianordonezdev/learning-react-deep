1a012e330a3b0cba2c42f74ce996b51063b300b3
Hernandez Ordonez <2383131@cognizant.com>
2025-01-16 14:00:27 -0600

    feature(final version on dispatching redux)

--- seven-app-redux/src/components/Counter.js
+++ seven-app-redux/src/components/Counter.js
@@ -1,13 +1,25 @@
 import classes from './Counter.module.css';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 const Counter = () => {
   const toggleCounterHandler = () => {};
+
+  const dispatch = useDispatch();
   const counter = useSelector(state => state.counter)
+
+  const handleAction = (action) => {
+    if (action === 'increment') dispatch({type: 'increment'})
+    else dispatch({type: 'decrement'})
+  };
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      <div W={classes.value}>{counter}</div>
+      <div className={classes.butts}>
+        <button onClick={() => handleAction('increment')}>Increment</button>
+        <button onClick={() => handleAction('')}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
