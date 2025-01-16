cc67c054172047e462c2a888d9e88846d75475d9
Hernandez Ordonez <2383131@cognizant.com>
2025-01-16 17:12:47 -0600

    feature(redux behaviour)

--- seven-app-redux/src/components/Counter.js
+++ seven-app-redux/src/components/Counter.js
@@ -2,25 +2,33 @@ import classes from './Counter.module.css';
 import { useSelector, useDispatch } from 'react-redux'
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
 
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter)
+  const showCounter = useSelector(state => state.showCounter)
 
   const handleAction = (action) => {
     if (action === 'increment') dispatch({type: 'increment'})
     else dispatch({type: 'decrement'})
   };
 
+  const handleIncrease = () => {
+    dispatch({type: 'increase', amount: 10})
+  }
+  const toggle = () => {
+    dispatch({type: 'toggle'})
+  }
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div W={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div className={classes.butts}>
         <button onClick={() => handleAction('increment')}>Increment</button>
+        <button onClick={handleIncrease}>Increase 10</button>
         <button onClick={() => handleAction('')}>Decrement</button>
       </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <button onClick={toggle}>Toggle Counter</button>
     </main>
   );
 };
